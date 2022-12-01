e4b5315b710a373ba1ffcadf12fe80a757aedc5b
bhoomikajethwani <bhoomsdj@gmail.com>
2022-11-30 19:22:09 -0700

    minor changes

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ import UsersController from "./controllers/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from "cors";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/tuiter'
-//mongoose.connect(CONNECTION_STRING);
+//const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect('mongodb://localhost:27017/tuiter');
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
 
-mongoose.connect('mongodb+srv://giuseppi:<supersecretpassword>@cluster0.yaes3td.mongodb.net/?retryWrites=true&w=majority');
+//mongoose.connect('mongodb+srv://giuseppi:<>@cluster0.yaes3td.mongodb.net/tuiter?retryWrites=true&w=majority');
 
 const app = express()
 app.use(cors())
